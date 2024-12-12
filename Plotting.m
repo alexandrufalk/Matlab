52a876cce2623ad5c93182fcc27069579e6f7177
Falk <alexandru.falk@yahoo.com>
2024-12-12 10:02:00 +0200

    3D Plotting added

--- Plotting.m
+++ Plotting.m
@@ -19,3 +19,17 @@ plot(x,y,'Color',[0,0.7,0.9])
 plot(x,y,'Color',[0,0.7,1.0])
 
 plot(x,y,'Color',[0,0.7,0.9]); title('2-D line plot'); xlabel('x-axis'); ylabel('values'); 
+
+%Plotting 3D data
+
+A = [1 5 0 7 0]
+
+B = [1 1 0 2 3 ]
+
+C = [5 8 7 9 6]
+
+plot3(A,B,C)
+
+stem3(A,B,C) 
+
+impz(A,B,C) 
