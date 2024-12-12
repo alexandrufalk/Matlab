c899b7be24eb9ab13e922ad277549ebdb92f2198
Falk <alexandru.falk@yahoo.com>
2024-12-12 08:53:26 +0200

    Plotting added

---
+++ Input_Output_commands.m
@@ -0,0 +1,34 @@
+input('Please neter a number'); 
+
+x = input('P{lease enter a number'); 
+
+x = input('Please enter a number','s'); 
+
+disp('Nouman Azam'); 
+
+a = 10
+
+disp(a)
+
+menu('Select Courses,''Matlab', 'c++', 'Java',);
+
+%The disp() function is simpler and more convenient for quick display of messages or variable values, while the fprintf() function provides more control over formatting for displaying output, especially when dealing with numerical data or generating formatted text
+ 
+fprintf('Hello World'); 
+
+fprintf('Hello World\n'); 
+
+x = 3
+ 
+fprintf('Hello World %d',x); 
+
+fprintf('Hello World %s \n',x); 
+
+A1 = [9.9, 9900];
+
+A2 = [8.8,  7.7 ; ...
+      8800, 7700];
+
+formatSpec = 'X is %4.2f meters or %8.3f mm\n';
+
+fprintf(formatSpec,A1,A2)
